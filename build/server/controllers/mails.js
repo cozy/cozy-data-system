5b1052d4a47c086dbc26835a6a4cabd7a56e5b71
poupotte <bellot.zoe@gmail.com>
2015-05-07 16:20:40 +0200

    built

--- build/server/controllers/mails.js
+++ build/server/controllers/mails.js
@@ -138,13 +138,13 @@ module.exports.sendFromUser = function(req, res, next) {
   } else {
     return db.view('cozyinstance/all', function(err, instance) {
       return db.view('user/all', function(err, users) {
-        var displayName, domain, mailOptions, ref, ref1;
+        var displayName, domain, mailOptions, ref, ref1, ref2;
         if ((instance != null ? (ref = instance[0]) != null ? ref.value.domain : void 0 : void 0) != null) {
           domain = instance[0].value.domain;
         } else {
           domain = 'your.cozy.io';
         }
-        if ((users != null ? (ref1 = users[0]) != null ? ref1.value.public_name : void 0 : void 0) != null) {
+        if (((users != null ? (ref1 = users[0]) != null ? ref1.value.public_name : void 0 : void 0) != null) && ((users != null ? (ref2 = users[0]) != null ? ref2.value.public_name : void 0 : void 0) != null) !== '') {
           displayName = users[0].value.public_name;
           displayName = displayName.toLowerCase().replace(' ', '-');
           displayName += "-";
